1e128f2e51783f306a17eaac6833eb9751665f3c
AMANBHATIA16 <amandeep.24scse1011218@galgotiasuniversity.ac.in>
2025-11-25 01:26:11 +0530

    Update Attendance.java

--- src/model/Attendance.java
+++ src/model/Attendance.java
@@ -1,1 +1,43 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Attendance {
+
+    private int id;
+    private int studentId;
+    private LocalDate date;
+    private String status;
+
+    public Attendance() {}
+
+    public Attendance(int id, int studentId, LocalDate date, String status) {
+        this.id = id;
+        this.studentId = studentId;
+        this.date = date;
+        this.status = status;
+    }
+
+    public Attendance(int studentId, LocalDate date, String status) {
+        this.studentId = studentId;
+        this.date = date;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
 
