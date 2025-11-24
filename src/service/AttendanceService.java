95ddd84a746cab595c38d96f77af097f3f28f271
AMANBHATIA16 <amandeep.24scse1011218@galgotiasuniversity.ac.in>
2025-11-25 01:28:39 +0530

    Update AttendanceService.java

--- src/service/AttendanceService.java
+++ src/service/AttendanceService.java
@@ -1,1 +1,39 @@
+package service;
+
+import dao.StudentDAO;
+import dao.AttendanceDAO;
+import model.Student;
+import model.Attendance;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class AttendanceService {
+
+    private StudentDAO studentDAO;
+    private AttendanceDAO attendanceDAO;
+
+    public AttendanceService(StudentDAO sDao, AttendanceDAO aDao) {
+        this.studentDAO = sDao;
+        this.attendanceDAO = aDao;
+    }
+
+    public boolean addStudent(String name, String course) {
+        Student s = new Student(name, course);
+        return studentDAO.addStudent(s);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentDAO.getAllStudents();
+    }
+
+    public boolean markAttendance(int id, String status) {
+        Attendance a = new Attendance(id, LocalDate.now(), status);
+        return attendanceDAO.markAttendance(a);
+    }
+
+    public List<Attendance> viewAttendance(int studentId) {
+        return attendanceDAO.getAttendanceByStudent(studentId);
+    }
+}
 
