910fe5a439a31b147a70a2d32ada68445deda2af
AMANBHATIA16 <amandeep.24scse1011218@galgotiasuniversity.ac.in>
2025-11-25 19:12:41 +0530

    Update StudentDAO.java

--- src/dao/StudentDAO.java
+++ src/dao/StudentDAO.java
@@ -1,1 +1,42 @@
+package dao;
+
+import model.Student;
+import java.sql.*;
+import java.util.*;
+
+public class StudentDAO {
+
+    public void addStudent(Student student) {
+        try {
+            Connection con = DBConnection.getConnection();
+            String query = "INSERT INTO students(name, course) VALUES (?, ?)";
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, student.getName());
+            ps.setString(2, student.getCourse());
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Student> getAllStudents() {
+        List<Student> list = new ArrayList<>();
+        try {
+            Connection con = DBConnection.getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM students");
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                list.add(new Student(
+                        rs.getInt("student_id"),
+                        rs.getString("name"),
+                        rs.getString("course")
+                ));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
 
