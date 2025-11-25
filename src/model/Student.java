4520eea59a4ae0f6776e3d6bd4b7d1a0ec7fb580
AMANBHATIA16 <amandeep.24scse1011218@galgotiasuniversity.ac.in>
2025-11-25 19:14:28 +0530

    Update Student.java

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,1 +1,24 @@
+package model;
+
+public class Student {
+
+    private int studentId;
+    private String name;
+    private String course;
+
+    public Student(int studentId, String name, String course) {
+        this.studentId = studentId;
+        this.name = name;
+        this.course = course;
+    }
+
+    public Student(String name, String course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public int getStudentId() { return studentId; }
+    public String getName() { return name; }
+    public String getCourse() { return course; }
+}
 
